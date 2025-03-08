23d8347a978f81a39221b28efc0fc0b8255844f2
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-03-08 23:34:50 +0400

    create db connection

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from config import settings
+
+DB_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+engine = create_async_engine(DB_URL)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
