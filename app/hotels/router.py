f873cdcc73edc68487ed7d4326e0024d19201b65
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-03-11 14:59:52 +0400

    add cache to get_booking and get_hotels_by_location

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from fastapi import APIRouter
+from fastapi_cache.decorator import cache
 
 from app.hotels.schemas import SHotelInfo
 from app.hotels.service import HotelDAO
@@ -9,6 +10,7 @@ router = APIRouter(prefix="/hotels", tags=["Hotels"])
 
 
 @router.get("/{location}")
+@cache(expire=10)
 async def get_hotels_by_location(location: str, date_from: date, date_to: date) -> list[SHotelInfo]:
     hotels = await HotelDAO.find_all(location, date_from, date_to)
     return hotels
